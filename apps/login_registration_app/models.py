84643231eb928d4f135d4c6f90faa58796d13584
Travis Alcantara <travis.alcantara@gmail.com>
2017-09-23 13:20:23 -0700

    added comments

--- apps/login_registration_app/models.py
+++ apps/login_registration_app/models.py
@@ -5,26 +5,35 @@ from django.db import models
 import re
 
 # Create your models here.
+#backend validation for User Model
 class UserManager(models.Manager):
     def validate_user(self, data):
         errors = []
         name_regex = re.compile(r'^[A-Za-z]+$')
         email_regex = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
         existing_email = User.objects.filter(email=data['email'])
+        #check if first name and last name are at least 2 characters
         if len(data['first_name']) < 2 or len(data['last_name']) < 2:
             errors.append('First name and last name must both be at least 2 characters')
+        #check if first name and last name are letters only
         if not name_regex.match(data['first_name']) or not name_regex.match(data['last_name']):
             errors.append('Both first and last name must be letters only!')
+        #check if email has been entered
         if len(data['email']) < 1:
             errors.append('Email is required')
+        #check if email is properly formatted utilizing regex
         if not email_regex.match(data['email']):
             errors.append('Email is improperly formatted')
+        #check if a user in the database with the entered email already exists
         if len(existing_email) > 0:
             errors.append('A user with that email already exist')
+        #check if password is at least 8 characters
         if len(data['password']) < 8:
             errors.append('Password must be at least 8 characters')
+        #check if password and password confirmation match
         if data['password'] != data['confirm_password']:
             errors.append('Password and password confirmation must match')
+        #check if day selected is valid according to the month, ie: Febuary cannot have 30 days, September cannot have 31 days, etc. Also checks for leap years.
         if data['birthday'].month != 1 and data['birthday'].month != 3 and data['birthday'].month != 5 and data['birthday'].month != 7 and data['birthday'].month != 8 and data['birthday'].month != 10 and data['birthday'].month != 12:
             if data['birthday'].month == 2:
                 if data['birthday'].year % 4 != 0:
--- apps/login_registration_app/views.py
+++ apps/login_registration_app/views.py
@@ -9,16 +9,20 @@ import bcrypt
 
 # Create your views here.
 def index(request):
+    #if there's a logged user, redirect to success page
     if 'logged_email' in request.session:
         return redirect(reverse('logreg:success'))
     registration_form = Registration_Form()
     login_form = Login_Form()
     return render(request, 'index.html', {'registration_form': registration_form, 'login_form': login_form})
 def register(request):
+    #do front end validation using form class
     form = Registration_Form(request.POST)
     if form.is_valid():
+        #do back end valdations using model validator
         errors = User.objects.validate_user(form.cleaned_data)
         if len(errors) > 0:
+            #log errors if model validator finds any
             for error in errors:
                 messages.error(request, error)
                 return redirect(reverse('logreg:index'))
@@ -33,6 +37,7 @@ def register(request):
             request.session['logged_email'] = email
             return redirect(reverse('logreg:success'))
     else:
+        #log errors if form class finds any
         for key in form.errors:
             for item in form.errors[key]:
                 string = key + ': ' + item
@@ -42,14 +47,18 @@ def success(request):
     logged_user = User.objects.get(email=request.session['logged_email'])
     return render(request, 'success.html', {'user': logged_user})
 def login(request):
+    #validate form with form class
     form = Login_Form(request.POST)
     if form.is_valid():
+        #grab user from database
         users = User.objects.filter(email = form.cleaned_data['email'])
         if len(users) < 1:
+            #if no user found, throw error
             messages.error(request, 'No user with that email found')
             return redirect(reverse('logreg:index'))
         else:
             password = form.cleaned_data['password']
+            #check if password entered matches password in database
             if bcrypt.checkpw(password.encode(), users[0].password.encode()):
                 request.session['logged_email'] = form.cleaned_data['email']
                 return redirect(reverse('logreg:success'))
@@ -57,11 +66,13 @@ def login(request):
                 messages.error(request, 'That password does not match what is on file')
                 return redirect(reverse('logreg:index'))
     else:
+        #log form class errors
         for key in form.errors:
             for item in form.errors[key]:
                 string = key + ': ' + item
                 messages.error(request, string)
                 return redirect(reverse('logreg:index'))
 def logout(request):
+    #clear session and redirect to index when logout is clicked
     request.session.clear()
     return redirect(reverse('logreg:index'))
